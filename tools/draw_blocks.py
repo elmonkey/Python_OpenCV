d5733fccf178bdefe9c4a139e8e39b82c5b36399
Carlos Torres <carlitos408@gmail.com>
2015-06-23 19:08:59 -0700

    Update draw_blocks.py

--- tools/draw_blocks.py
+++ tools/draw_blocks.py
@@ -1,4 +1,4 @@
-def drawblocks(image, n=6):
+def drawblocks(image, n=6, thickness=1):
     """
     Draws lines on the perimeter and over the n x n blocks. cv2 uses backwards coords
             (0,0)______(w,0)
@@ -9,12 +9,17 @@ def drawblocks(image, n=6):
             (0,h)_______(w,h)
     
     input: 
-        image = 1L or 3L numpy array, uint8 [0,255]
-        n = block dimensions (n x n). For example, n=6 -> 6x6 = 36 blocks 
-    output: image with grid dron on it 
-    
+        image := numpy array, 1L or 3L uint8 [0,255]
+        n := int, block dimensions (n x n). For example, n=6 -> 6x6 = 36 blocks
+        thickness:= int, line thickness
+    output: image with grid dron on it
     """
+    # Line thickness
+    
+    
     # Colors
+    yellow =(102,255,255)
+    orange =(0,128,255)
     blue = (255,0,0)
     red  = (0,0,255)
     green= (0,255,0)
@@ -25,24 +30,28 @@ def drawblocks(image, n=6):
         im = image
         
     h,w=im.shape[:2]
+     
     
-    # DRAW Perimeter                                      
-    cv2.line(im, (0,0), (w-1,0),     green, thickness=4) # top  __
-    cv2.line(im, (0,0), (0,h-1),     green, thickness=4) # left |
-    cv2.line(im, (0,h-1), (w-1,h-1), green, thickness=4) # bottom __
-    cv2.line(im, (w-1,0), (w-1,h-1), green, thickness=4) # rite |    
-    
-    if n > 1:
+    if n > 0:
         # The lists of line intervals
-        wb = np.uint8(np.floor(w/n)) # horizontal step, for vertical lines
-        hb = np.uint8(np.floor(h/n)) # vertical step, for horizontal lines
-        y = np.arange(wb,w,wb)
-        x = np.arange(hb,h,hb)
+        wb = int(np.floor(w/n)) # horizontal step, for vertical lines
+        hb = int(np.floor(h/n)) # vertical step, for horizontal lines
+        y = range(0,h,hb)
+        x = range(0,w,wb)
         
         # Draw the grid
-        for i in range(len(x)-1):
-            cv2.line(im, (x[i], 0), (x[i], w), blue, thickness=4) # vertical lines
+        color = orange
+        for i in range(len(x)):
+            cv2.line(im, (x[i], 0), (x[i], h), color=color,  thickness=thickness) # vertical lines
         for i in range(len(y)-1):
-            cv2.line(im, (0, y[i]), (h, y[i]), red,  thickness=4) # horizontal lines 
+            cv2.line(im, (0, y[i]), (w, y[i]), color=color,  thickness=thickness) # horizontal lines
+
+    # DRAW Perimeter
+    #color=red # change the color
+    cv2.line(im, (0,0), (w-1,0),     color=color, thickness=thickness) # top  __
+    cv2.line(im, (0,0), (0,h-1),     color=color, thickness=thickness) # left |
+    cv2.line(im, (0,h-1), (w-1,h-1), color=color, thickness=thickness) # bottom __
+    cv2.line(im, (w-1,0), (w-1,h-1), color=color, thickness=thickness) # rite |   
+
     return im
 #drawblocks
